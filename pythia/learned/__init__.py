2ca5985cb475497623f7ca2bbbd8506b2a0911cb
Matthew Spellings <mspells@umich.edu>
2020-01-21 14:43:39 -0600

    Fix missing __init__.py due to .gitignore

---
+++ pythia/learned/__init__.py
@@ -0,0 +1,9 @@
+"""
+The `pythia.learned` module implements building blocks for
+creating learned representations of particle systems. They are
+implemented within the `keras <https://keras.io>`_ framework for ease
+of integration into standard workflows.
+"""
+
+from . import bonds # noqa
+from . import spherical_harmonics # noqa
